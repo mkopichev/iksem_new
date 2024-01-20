d1fe31d6385639cd717dc4a03dbe8cba6d8effbf
Michael <mmkopychev@etu.ru>
2024-01-20 16:59:08 +0300

    bluetooth and map screens drawn

--- iksem_software/app/src/main/java/com/iksem/ui/bluetooth/BluetoothFragment.java
+++ iksem_software/app/src/main/java/com/iksem/ui/bluetooth/BluetoothFragment.java
@@ -5,9 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
 import com.iksem.databinding.FragmentBluetoothBinding;
 
 public class BluetoothFragment extends Fragment {
