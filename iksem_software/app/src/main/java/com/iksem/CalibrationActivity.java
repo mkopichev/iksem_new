c3e7b82287522b7afdddfa304d8bd30d19a607f6
Michael <mmkopychev@etu.ru>
2024-02-07 22:52:05 +0300

    try to show calibration activity

--- iksem_software/app/src/main/java/com/iksem/CalibrationActivity.java
+++ iksem_software/app/src/main/java/com/iksem/CalibrationActivity.java
@@ -1,15 +1,22 @@
 package com.iksem;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class CalibrationActivity extends AppCompatActivity {
 
+    MaterialToolbar materialToolbar;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         setContentView(R.layout.activity_calibration);
+        materialToolbar = findViewById(R.id.calibration_toolbar);
     }
 }
--- iksem_software/app/src/main/java/com/iksem/ui/settings/SettingsFragment.java
+++ iksem_software/app/src/main/java/com/iksem/ui/settings/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.iksem.ui.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.button.MaterialButton;
+import com.iksem.CalibrationActivity;
 import com.iksem.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
@@ -32,11 +34,7 @@ public class SettingsFragment extends Fragment {
 
         MaterialButton calibration = binding.settingsCalibrationButton;
 
-        calibration.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
+        calibration.setOnClickListener(v -> startActivity(new Intent(getActivity(), CalibrationActivity.class)));
 
         return root;
     }
